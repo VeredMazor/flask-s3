b751f9ca1d151a8e3ee9044433c87b1f7dfdbc12
Vered Mazor <veredm582@gmail.com>
2024-05-22 00:02:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
 with app.app_context():
     db.create_all()
